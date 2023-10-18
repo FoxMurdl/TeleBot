86bedb12eb1c57d68f68f2b7ba2118028fecf161
Evgeny <128634699+FoxMurdl@users.noreply.github.com>
2023-10-18 22:58:32 +0300

    done
    Готовый телеграмм бот

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = '6644541571:AAH16Gjiowjc3W0iJRD6CItnSGcTRrmrCzQ'
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'лира': 'TRY',
+    'тенге': 'KZT'
+    }
