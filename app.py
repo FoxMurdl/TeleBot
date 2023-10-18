6674e8637aedc7a59e661e443f82e382c3344d9a
Evgeny <128634699+FoxMurdl@users.noreply.github.com>
2023-10-19 01:47:11 +0300

    upd
    upd code

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def convert(message: telebot.types.Message):
     except Exception as e:
         bot.reply_to(message, f'Не удалость обработать команду\n{e}')
     else:
-        text = f'Цена {amount} {quote} в {base} - {total_base}'
+        text = f'Переводим {amount}  {quote} в {base} = {total_base}'
         bot.send_message(message.chat.id, text)
 
 
